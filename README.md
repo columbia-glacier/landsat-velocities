7b09e072bee5476c514845df48ce27cb6b542f04
Ethan Welty <ethan.welty@gmail.com>
2017-05-18 12:32:31 -0600

    Start work on scraping acquisiting times

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 LANDSAT Velocities
 =======================
-*by Robert McNabb*
+by Robert McNabb
 
-<span style="background-color: #FFFF00">This is a metadata-only repository. Data are only available from the author.</span>
+*This is a metadata-only repository. Data are only available from the author.*
 
-### Bob McNabb, email: 2017-03-14
+## Citation
+
+> Robert W. McNabb, Regina Hock, and Matthias Huss (2015). Variations in Alaska Tidewater Glacier Frontal Ablation, 1985–2013. Journal of Geophysical Research: Earth Surface 120, 120-136. doi:[10.1002/2014JF003276](https://doi.org/10.1002/2014JF003276)
+
+## Source data
 
 There are three folders corresponding to the landsat sensor used (TM, ETM, OLI), and inside each folder there's a bunch of `*.tar.gz` files with the actual data.
 
@@ -30,7 +34,7 @@ There are three folders corresponding to the landsat sensor used (TM, ETM, OLI),
 
 They haven't been filtered at all, but I did cull any vectors that were more than twice the mean plus twice the standard deviation of the on-glacier velocities. Some of the ETM slc-off scenes might need some work, but fortunately Columbia sits right towards the middle of most of the scenes, so the striping isn't too bad for the terminus region.
 
-## Datums
+### Datums
 
 All GeoTIFFs and Shapefiles are in WGS84 UTM Zone 6N. Date ranges are based on dates only (e.g. 19940101_19940102 = 24 hours). Time can be gleaned from metadata returned by URL queries like:
 
--- scripts/process.R
+++ scripts/process.R
@@ -1,15 +1,52 @@
 library(magrittr)
 
+# 3491: ETM+ Pan Mosaics (1999-2003)
+# 3492: TM Mosaics (1984-1997)
+# 2732: ETM+ Pan (1999-2003)
+# 2731: ETM+ (1999-2003)
+# 2733: TM (1984-1997)
+# 2871: MSS 1-5 (1972-1987)
+# 3211: Sys ETM+ L1G
+# 
+
 # Retrieve granules (LANDSAT image ids) from shapefiles
 files <- "sources/tm" %>%
   list.files(pattern = "\\.dbf$", recursive = TRUE, full.names = TRUE)
+
 granules <- lapply(files, function(file) {
   print(file)
   file %>%
     foreign::read.dbf(as.is = TRUE) %>%
     extract(1, c("img1", "img2")) %>%
     cbind(file)
-})
+}) %>%
+  do.call("rbind", .)
 
+granule_list <- unique(unlist(granules[c("img1", "img2")]))
+parameters <- c("acquisition_date", "start_time", "stop_time")
+url <- "https://earthexplorer.usgs.gov/form/metadatalookup/"
+
+granule_dates <- lapply(granule_list, function(granule) {
+  print(granule)
+  query <- list(collection_id = 2731, entity_id = granule)
+  response <- httr::GET(url, query = query)
+  xml <- xml2::read_html(rawToChar(response$content))
+  rows <- xml %>%
+    xml2::xml_find_all("//table[@id = 'metadataTable']/tbody/tr")
+  ind <- rows %>%
+    xml2::xml_find_all("td[1]/a") %>%
+    xml2::xml_attr("href") %>%
+    gsub("^.*#([a-zA-Z0-9_]+).*$", "\\1", .) %>%
+    match(parameters, .)
+  values <- rows %>%
+    extract(ind) %>%
+    xml2::xml_find_all("td[2]") %>%
+    xml2::xml_text()
+  names(values) <- parameters
+  as.data.frame(t(values))
+}) %>%
+  do.call("rbind", .)
+  
+# Write granules to file
 # Retrieve image capture times from granules
 # Rename rasters
